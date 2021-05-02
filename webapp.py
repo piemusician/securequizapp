f0e318d31f1eb7b0677244119702cc4366ea4993
Melany Nunez <melanynunez@Melanys-MacBook-Pro.local>
2021-05-01 19:36:12 -0700

    done

--- webapp.py
+++ webapp.py
@@ -12,13 +12,22 @@ app = Flask(__name__)
 app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.  
                                      #The value should be set in Heroku (Settings->Config Vars).  
                                      #To run locally, set in env.sh and include that file in gitignore so the secret key is not made public.
+def store_Answers(answer, x):
+    print("yeet")
+    if 'answer' + str(x) not in session:
+        session['answer' + str(x)] = answer
+    else:
+        return render_template('page' + str(x) + '.html')
 
+def my_Function(x, y):
+    print(x + " " + y)
+    
 @app.route('/')
 def renderMain():
     return render_template('home.html')
 
-@app.route('/startOver')
-def startOver():
+@app.route('/retakeQuiz')
+def retakeQuiz():
     session.clear() #clears variable values and creates a new session
     return redirect(url_for('renderMain')) # url_for('renderMain') could be replaced with '/'
 
@@ -28,14 +37,27 @@ def renderPage1():
 
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
-    session["firstName"]=request.form['firstName']
-    session["lastName"]=request.form['lastName']
+    print("yote")
+    store_Answers(request.form['answer1'], 1)
     return render_template('page2.html')
 
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
-    session["favoriteColor"]=request.form['favoriteColor']
+    store_Answers(request.form['answer2'], 2)
     return render_template('page3.html')
-    
+
+@app.route('/final', methods =['GET', 'POST'])
+def renderFinal():
+    score=0
+    store_Answers(request.form['answer3'], 3)
+    print('Biillie Eyelasher')
+    if session['answer1'] == 'Watches' or 'watches':
+        score = score +1       
+    if session['answer2'] == 'December 18, 2001' or '12/18/01':
+        score = score +1
+    if session['answer3'] == 'Ocean Eyes' or 'ocean eyes':
+        score = score +1
+    return render_template('final.html' , finalScore=score)
+
 if __name__=="__main__":
     app.run(debug=False)
